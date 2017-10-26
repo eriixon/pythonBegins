a4b7f277faee5ef20aa2019821f77a8689065005
eriixon <radkovsn@gmail.com>
2017-10-25 18:11:16 -0600

    Client

--- Metric/client.py
+++ Metric/client.py
@@ -1,5 +1,5 @@
 import time
-import asyncio
+import socket
 
 
 class ClientError(Exception):
@@ -13,38 +13,37 @@ class Client:
         self.port = port
         self.timeout = timeout
 
-    async def tcp_echo_client_put(self, message, loop):
-        reader, writer = await asyncio.open_connection(self.host, self.port, loop=loop)
-        print("send: %r" % message)
-        writer.write(message.encode())
-        writer.close()
-        data = await reader.read(100)
-        print('Received: %r' % data.decode())
-        await asyncio.sleep(1.0)
-        return data
+    def server_connect(self, message):
+        with socket.create_connection((self.host, self.port),self.timeout) as s:
+            try:
+                s.sendall(message)
+                s.timeout(1)
+                data = s.recv(1024)
+            except socket.timeout:
+                print("send data timeout")
+            except socket.error as ex:
+                print("send data error:", ex)
+            return data.decode("utf8")
+
 
     def put(self, key, value, timestamp=None):
+
         if timestamp == None:
             timestamp = str(int(time.time()))
         message = "put {0} {1} {2}\n\n".format(key, value, timestamp)
-        loop = asyncio.get_event_loop()
-        data = loop.run_until_complete(self.tcp_echo_client_put(message, loop))
-        loop.close()
+        data = self.server_coonect(message)
         if data != "ok\n\n":
             raise ClientError
 
     def get(self, value):
         message = "get {0}\n".format(value)
-        loop = asyncio.get_event_loop()
-        data = loop.run_until_complete(self.tcp_echo_client_put(message, loop))
-        loop.close()
+        data = self.server_coonect(message)
         if data == "ok\n\n":
             return {}
         return data
 
-    #Клиент получает данные в текстовом виде, метод get должен возвращать словарь с полученными ключами с сервера.
-    #Значением ключа в словаре является список кортежей[(timestamp, metric_value), ...], отсортированный
-    #по timestamp от меньшего к большему.
-    #Значение timestamp должно быть преобразовано к целому числу int.
-    #Значение метрики metric_value нужно преобразовать к числу с плавающей точкой float.
-
+        # Клиент получает данные в текстовом виде, метод get должен возвращать словарь с полученными ключами с сервера.
+        # Значением ключа в словаре является список кортежей[(timestamp, metric_value), ...], отсортированный
+        # по timestamp от меньшего к большему.
+        # Значение timestamp должно быть преобразовано к целому числу int.
+        # Значение метрики metric_value нужно преобразовать к числу с плавающей точкой float.
